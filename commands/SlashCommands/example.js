9eeabc7b09affab2a47e115c4c26dc6b6d8591c8
Miky88 <mikyottantotto.88@gmail.com>
2021-05-28 14:43:55 +0200

    Ephemeral messages

--- commands/SlashCommands/example.js
+++ commands/SlashCommands/example.js
@@ -1,5 +1,9 @@
+/**
+ * @param {import("discord.js").Client & { commands: Map, PluginManager: import("../modules/PluginManager") }} client
+ * @param {import("discord.js").CommandInteraction} interaction
+ */
 exports.run = async (client, interaction, args) => {
-    interaction.reply(args["input"])
+    interaction.reply(args["input"], {ephemeral: true})
 };
 
 exports.config = {
