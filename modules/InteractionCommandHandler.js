e57dd7ffc1fde3191ead7638ff3501bfa757d2ed
Just1diaxx <157634021+Just1diaxx@users.noreply.github.com>
2024-10-12 15:25:34 +0200

    si

--- modules/InteractionCommandHandler.js
+++ modules/InteractionCommandHandler.js
@@ -33,7 +33,6 @@ module.exports = class InteractionCommandHandler extends Module {
 
             // Required Flag check
             if (cmd.config.requiredFlag.length > 0) {
-                console.log(interaction.user.data)
                 let flags = client.database.getFlags(interaction.user.id)
                 let flag = cmd.config.requiredFlag.find(f => !flags.includes(f))
                 if (flag) return interaction.reply({ content: `:no_entry: You don't have required flag **${flag}** to perform this command.`, ephemeral: true})
